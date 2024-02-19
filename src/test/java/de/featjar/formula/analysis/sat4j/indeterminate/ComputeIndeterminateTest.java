dfd7e0b49e79f3ae332b91e11f3d8380160482bf
pascalfoerster <pascal.foerster@uni-ulm.de>
2024-02-19 12:30:38 +0100

    update tests

--- src/test/java/de/featjar/formula/analysis/sat4j/indeterminate/ComputeIndeterminateTest.java
+++ src/test/java/de/featjar/formula/analysis/sat4j/indeterminate/ComputeIndeterminateTest.java
@@ -20,12 +20,15 @@
  */
 package de.featjar.formula.analysis.sat4j.indeterminate;
 
+import static de.featjar.base.computation.Computations.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import de.featjar.Common;
 import de.featjar.base.computation.Computations;
 import de.featjar.base.computation.IComputation;
 import de.featjar.base.data.Pair;
+import de.featjar.base.data.Result;
+import de.featjar.base.tree.Trees;
 import de.featjar.formula.analysis.VariableMap;
 import de.featjar.formula.analysis.bool.BooleanAssignment;
 import de.featjar.formula.analysis.bool.BooleanClauseList;
@@ -33,25 +36,32 @@ import de.featjar.formula.analysis.bool.BooleanRepresentationComputation;
 import de.featjar.formula.analysis.bool.IBooleanRepresentation;
 import de.featjar.formula.analysis.mig.solver.MIGBuilder;
 import de.featjar.formula.analysis.mig.solver.ModalImplicationGraph;
+import de.featjar.formula.analysis.sat4j.ComputeCoreDeadVariablesSAT4J;
+import de.featjar.formula.analysis.sat4j.solver.SAT4JSolutionSolver;
 import de.featjar.formula.structure.formula.IFormula;
-import de.featjar.formula.structure.formula.connective.BiImplies;
+import de.featjar.formula.structure.formula.connective.*;
+import de.featjar.formula.structure.formula.predicate.Literal;
+import de.featjar.formula.transform.CNFSlicer;
 import de.featjar.formula.transformer.ComputeCNFFormula;
 import de.featjar.formula.transformer.ComputeNNFFormula;
 
 import java.nio.channels.Pipe;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import de.featjar.formula.visitor.CoreDeadSimplifier;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ComputeIndeterminateTest extends Common {
 
     private static ArrayList<IComputation<IFormula>> formulas = new ArrayList<>();
-    private static ComputeCNFFormula cnfC ;
+
     private static ArrayList<IComputation<IBooleanRepresentation>> clauses= new ArrayList<>();
     private static ArrayList<VariableMap> variables = new ArrayList<>();
     private static ArrayList<BooleanAssignment> hiddenVariables = new ArrayList<>();
@@ -63,7 +73,7 @@ public class ComputeIndeterminateTest extends Common {
 
     @BeforeAll
     static void createTests(){
-        for(int i = 1; i < 5; i++){
+        for(int i = 1; i < 6; i++){
             Pair<IFormula,Pair<List<String>,List<BiImplies>>> model = loadHiddenModel("testFeatureModels/testIndeterminateFeatureModels/model"+i+".xml");
             IComputation<IFormula> formula = Computations.of(model.getKey());
             formulas.add(formula);
@@ -78,10 +88,9 @@ public class ComputeIndeterminateTest extends Common {
             biImplies.add(model.getValue().getValue());
 
         }
+        IFormula cnfC = formulas.get(0).map(ComputeNNFFormula::new)
+                .map(ComputeCNFFormula::new).compute();
 
-
-        cnfC = formulas.get(3).map(ComputeNNFFormula::new)
-                .map(ComputeCNFFormula::new);
 
         coreFeatures.add( new BooleanAssignment(variables.get(1).get("h").get()));
         deadFeatures.add(  new BooleanAssignment(variables.get(1).get("g").get()));
@@ -107,7 +116,7 @@ public class ComputeIndeterminateTest extends Common {
         List<String> indeterminate2 = result2.streamValues()
                 .map(v -> variables.get(0).get(v.getKey()).get()).sorted()
                 .collect(Collectors.toCollection(ArrayList::new));
-        assertEquals(Stream.of("d","e","f").sorted().collect(Collectors.toList()), indeterminate2);
+        assertEquals(Stream.of("d","e","f","m").sorted().collect(Collectors.toList()), indeterminate2);
 
         BooleanAssignment result3 = clauses.get(2).cast(BooleanClauseList.class)
                 .map(ComputeIndeterminate::new)
@@ -124,12 +133,12 @@ public class ComputeIndeterminateTest extends Common {
     void formulaOnlyIndeterminateSlicingAnalysis() {
         BooleanAssignment result = clauses.get(0).cast(BooleanClauseList.class)
                 .map(ComputeIndeterminateSlicing::new)
-                .set(ComputeIndeterminate.VARIABLES_OF_INTEREST, hiddenVariables.get(0))
+                .set(ComputeIndeterminateSlicing.VARIABLES_OF_INTEREST, hiddenVariables.get(0))
                 .compute();
         List<String> indeterminate = result.streamValues()
                 .map(v -> variables.get(0).get(v.getKey()).get()).sorted()
                 .collect(Collectors.toCollection(ArrayList::new));
-        assertEquals(Stream.of("d","e","f").sorted().collect(Collectors.toList()), indeterminate);
+        assertEquals(Stream.of("d","e","f","m").sorted().collect(Collectors.toList()), indeterminate);
     }
 
 
@@ -172,10 +181,10 @@ public class ComputeIndeterminateTest extends Common {
 
         assertEquals(0, indeterminate.size());
         BooleanAssignment hiddenVariables2 = new PreprocessIffV2(formulas.get(2))
-                .set(PreprocessIff.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(2))
-                .set(PreprocessIff.VARIABLE_MAP, variables.get(2))
-                .set(PreprocessIff.CORE_FEATURE, coreFeatures.get(1))
-                .set(PreprocessIff.DEAD_FEATURE, deadFeatures.get(1))
+                .set(PreprocessIffV2.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(2))
+                .set(PreprocessIffV2.VARIABLE_MAP, variables.get(2))
+                .set(PreprocessIffV2.CORE_FEATURE, coreFeatures.get(1))
+                .set(PreprocessIffV2.DEAD_FEATURE, deadFeatures.get(1))
                 .compute();
         List<String> indeterminate2 = hiddenVariables2.streamValues()
                 .map(v ->  variables.get(2).get(v.getKey()).get()).sorted()
@@ -187,10 +196,10 @@ public class ComputeIndeterminateTest extends Common {
     @Test
     void preprocessSort(){
         BooleanAssignment hiddenVariables = new PreprocessIffSort(formulas.get(1))
-                .set(PreprocessIff.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(1))
-                .set(PreprocessIff.VARIABLE_MAP, variables.get(1))
-                .set(PreprocessIff.CORE_FEATURE, coreFeatures.get(0))
-                .set(PreprocessIff.DEAD_FEATURE, deadFeatures.get(0))
+                .set(PreprocessIffSort.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(1))
+                .set(PreprocessIffSort.VARIABLE_MAP, variables.get(1))
+                .set(PreprocessIffSort.CORE_FEATURE, coreFeatures.get(0))
+                .set(PreprocessIffSort.DEAD_FEATURE, deadFeatures.get(0))
                 .compute();
         List<String> indeterminate = hiddenVariables.streamValues()
                 .map(v ->  variables.get(1).get(v.getKey()).get()).sorted()
@@ -210,16 +219,17 @@ public class ComputeIndeterminateTest extends Common {
         List<String> indeterminate = hiddenVariables.streamValues()
                 .map(v ->  variables.get(3).get(v.getKey()).get()).sorted()
                 .collect(Collectors.toCollection(ArrayList::new));
-        assertEquals(List.of("k"), indeterminate);
+        assertEquals(List.of("k","m"), indeterminate);
     }
 
     @Test
     void preprocessBiComplicated(){
         BooleanAssignment hiddenVariables = new PreprocessIffComp(formulas.get(2))
-                .set(PreprocessIff.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(2))
-                .set(PreprocessIff.VARIABLE_MAP, variables.get(2))
-                .set(PreprocessIff.CORE_FEATURE, coreFeatures.get(1))
-                .set(PreprocessIff.DEAD_FEATURE, deadFeatures.get(1))
+                .set(PreprocessIffComp.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(2))
+                .set(PreprocessIffComp.VARIABLE_MAP, variables.get(2))
+                .set(PreprocessIffComp.EXPRESSION_MAX_LENGTH,5)
+                .set(PreprocessIffComp.CORE_FEATURE, coreFeatures.get(1))
+                .set(PreprocessIffComp.DEAD_FEATURE, deadFeatures.get(1))
                 .compute();
         List<String> indeterminate = hiddenVariables.streamValues()
                 .map(v ->  variables.get(2).get(v.getKey()).get()).sorted()
@@ -230,10 +240,11 @@ public class ComputeIndeterminateTest extends Common {
     @Test
     void preprocessBiComplicatedSort(){
         BooleanAssignment hiddenVariables = new PreprocessIffCompSort(formulas.get(2))
-                .set(PreprocessIff.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(2))
-                .set(PreprocessIff.VARIABLE_MAP, variables.get(2))
-                .set(PreprocessIff.CORE_FEATURE, coreFeatures.get(1))
-                .set(PreprocessIff.DEAD_FEATURE, deadFeatures.get(1))
+                .set(PreprocessIffCompSort.VARIABLES_OF_INTEREST, ComputeIndeterminateTest.hiddenVariables.get(2))
+                .set(PreprocessIffCompSort.VARIABLE_MAP, variables.get(2))
+                .set(PreprocessIffCompSort.EXPRESSION_MAX_LENGTH,5)
+                .set(PreprocessIffCompSort.CORE_FEATURE, coreFeatures.get(1))
+                .set(PreprocessIffCompSort.DEAD_FEATURE, deadFeatures.get(1))
                 .compute();
         List<String> indeterminate = hiddenVariables.streamValues()
                 .map(v ->  variables.get(2).get(v.getKey()).get()).sorted()
@@ -242,7 +253,80 @@ public class ComputeIndeterminateTest extends Common {
     }
 
     @Test
+    void testCoreDeadSimplifier() {
+        BooleanAssignment assignment = await(clauses.get(4).cast(BooleanClauseList.class).map(ComputeCoreDeadVariablesSAT4J::new));
+        IFormula formula = formulas.get(4).compute();
+        formula.getChildren().get(26).getChildren().get(1).replaceChild(1,new Literal(false,"h"));
+
+        Result<IFormula> formulaResult = Reference.mutateClone(formula, reference -> Trees.traverse(reference, new CoreDeadSimplifier(assignment.toValueName(variables.get(4)))));
+        IFormula result = new And(
+                new Or(new Literal("i"), new Literal("j")),
+                new BiImplies(new Literal("k"), new Not(new Literal("l"))),
+                new BiImplies(new Literal("l"), new Not(new Literal("k"))),
+                new Implies(new Literal("n"), new Literal("m")),
+                new Implies(new Literal("o"), new Literal("m")),
+                new Not(new And(new Literal("f"), new Literal("g"))),
+                new Or(new Literal("f"), new Literal("g")),
+                new Or(new Literal("g"), new Literal("f")),
+                new Or(new Literal("f"), new Literal("i")),
+                new Or(new Literal("f"), new Literal("g")),
+                new Or(new Literal("f"), new Literal("g")),
+                new Not(new And(new Literal("n"), new And(new Literal("o"),new Literal("h")))),
+                new BiImplies(new Literal("m"), new Literal("h")),
+                new Or(new Literal("f"), new Literal("g")),
+                new Or(new Literal("f"), new Literal("g"))
+
+
+                );
+        assert result.equalsTree(formulaResult.get());
+
+
+    }
+
+    @Test
     void testBiImplicationTransformer(){
+        IFormula cnf = formulas.get(3).map(ComputeNNFFormula::new).map(ComputeCNFFormula::new).compute();
+        List<BiImplies> biImpliesList1 = new ComputeBiImplicationFormula(cnf,variables.get(3))
+                .set(ComputeBiImplicationFormula.ALREADY_EXISTING_BI_IMPL,biImplies.get(3))
+                .set(ComputeBiImplicationFormula.MAXIMUM_CLAUSE_SIZE,2)
+                .compute();
+        List<BiImplies> results = new ArrayList<>();
+        results.add(new BiImplies(new Literal("b"),new Literal("d")));
+        assertEquals(results.size(),biImpliesList1.size());
+        for(BiImplies biImplies: biImpliesList1) {
+            assert results.stream().anyMatch(biImplies1 -> biImplies1.equalsTree(biImplies));
+        }
+        List<BiImplies> biImpliesList2 = new ComputeBiImplicationFormula(cnf,variables.get(3))
+                .set(ComputeBiImplicationFormula.ALREADY_EXISTING_BI_IMPL,biImplies.get(3))
+                .set(ComputeBiImplicationFormula.MAXIMUM_CLAUSE_SIZE,4)
+                .compute();
+        results.add(new BiImplies(new Literal("m"),new Or(new Literal("e"),new Literal("i"))));
+        results.add(new BiImplies(new Literal("q"),new Or(new Literal("s"), new Literal("t"),new Literal("r"))));
+        assertEquals(results.size(),biImpliesList2.size());
+        for(BiImplies biImplies: biImpliesList2) {
+            assert results.stream().anyMatch(biImplies1 -> biImplies1.equalsTree(biImplies));
+        }
+        results.add(new BiImplies(new Literal("b"),new Literal("a")));
+        results.add(new BiImplies(new Literal("n"), new Or(new Literal("o"),new Literal("p"))));
+        results.add(new BiImplies(new Literal(false,"r"),new Or(new Literal("s"),new Literal("t"),new Literal(false,"q"))));
+        results.add(new BiImplies(new Literal(false,"s"),new Or(new Literal("t"),new Literal(false,"q"),new Literal("r"))));
+        results.add(new BiImplies(new Literal(false,"t"),new Or(new Literal("s"),new Literal(false,"q"),new Literal("r"))));
+        List<BiImplies> biImpliesList3 = new ComputeBiImplicationFormula(cnf,variables.get(3))
+                .set(ComputeBiImplicationFormula.MAXIMUM_CLAUSE_SIZE,4)
+                .compute();
+        assertEquals(results.size(),biImpliesList3.size());
+        for(BiImplies biImplies: biImpliesList3) {
+            assert results.stream().anyMatch(biImplies1 -> biImplies1.equalsTree(biImplies));
+        }
+        results.add(new BiImplies(new Literal(false,"u"),new Or(new Literal("v"),new Literal("w"),new Literal("x"),new Literal("y"),new Literal("z"))));
+        List<BiImplies> biImpliesList4 = new ComputeBiImplicationFormula(cnf,variables.get(3))
+                .set(ComputeBiImplicationFormula.MAXIMUM_CLAUSE_SIZE,6)
+                .compute();
+        assertEquals(results.size(),biImpliesList4.size());
+        for(BiImplies biImplies: biImpliesList4) {
+            assert results.stream().anyMatch(biImplies1 -> biImplies1.equalsTree(biImplies));
+        }
+
 
     }
 
